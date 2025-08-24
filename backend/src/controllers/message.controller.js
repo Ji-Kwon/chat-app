81875ec77ea8b50798f93ec6b2bd2476607b720c
Ji-Kwon <jacob.wilkinson16@gmail.com>
2025-08-23 23:25:51 -0400

    final commit

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,5 +1,7 @@
 import Message from "../models/message.model.js"
 import User from "../models/user.model.js"
+import cloudinary from "../lib/cloudinary.js"
+import { getReceiverSocketId, io } from "../lib/socket.js"
 
 export const getUsersForSidebar = async (req, res) => {
     try {
@@ -52,8 +54,12 @@ export const sendMessage = async (req, res) => {
 
         await newMessage.save()
 
-        //todo add realtime functionality (socket.io)
-        
+        const receiverSocketId = getReceiverSocketId (receiverId)
+        if(receiverSocketId){
+            io.to(receiverSocketId).emit("newMessage", newMessage)
+        }
+
+
         res.status(201).json(newMessage)
     } catch (error) {
         console.error("Error in sendMessage controller:", error.message)
